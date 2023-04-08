c1d6005f1b3fd0a821281569662445e5458f382a
dlgusdk <83761937+dlgusdk@users.noreply.github.com>
2023-04-08 22:12:26 +0900

    Day10

--- src/Ex_74.java
+++ src/Ex_74.java
@@ -1,1 +1,45 @@
 
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int leaderCapacity;
+    static int memberCapacity;
+
+    public static int needMember(int customerNum) {
+        if(customerNum <= 0) {
+            return 0;
+        }
+        
+        if(customerNum % memberCapacity == 0) {
+            return customerNum / memberCapacity;
+        }
+        else {
+            return (customerNum / memberCapacity) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        long answer = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+        int[] customerArr = new int[1000000];
+
+        for(int i = 0; i < n; i++) {
+            customerArr[i] = scan.nextInt();
+        }
+
+        leaderCapacity = scan.nextInt();
+        memberCapacity = scan.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            answer++;
+
+            answer += needMember(customerArr[i] - leaderCapacity);
+        }
+
+        System.out.println(answer);
+    }
+}
