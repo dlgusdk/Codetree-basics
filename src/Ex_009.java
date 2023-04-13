4253bd9176d5aa82c242545516ddb333bf46ccb8
dlgusdk <83761937+dlgusdk@users.noreply.github.com>
2023-04-13 23:52:23 +0900

    Day1

---
+++ src/Ex_009.java
@@ -0,0 +1,16 @@
+/*Total days in Year
+365
+Circumference rate
+3.1415926535
+를 출력하는 프로그램을 작성해보세요.*/
+
+public class Ex_09 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Total days in Year");
+        System.out.println("365");
+        System.out.print("Circumference rate\n3.1415926535"); 
+	}
+
+}
